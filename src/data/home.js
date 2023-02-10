0dd6589951037717499937110eef9f7df1c57c73
merb <michael.erb@erbwebdev.com>
2023-02-10 13:38:12 -0700

    feat: move data to its own file

---
+++ src/data/home.js
@@ -0,0 +1,118 @@
+import koaLogo from '@imgs/tech/koajs_logo.svg'
+import vueLogo from '@imgs/tech/vuejs.svg'
+import postcssLogo from '@imgs/tech/postcss_logo.svg'
+import htmlLogo from '@imgs/tech/html5-logo.svg'
+import cssLogo from '@imgs/tech/css3-logo.svg'
+import jsLogo from '@imgs/tech/javascript.svg'
+import vscodeLogo from '@imgs/tech/vscode_logo.svg'
+import zshLogo from '@imgs/tech/zsh color logo.svg'
+import dockerLogo from '@imgs/tech/docker_icon.svg'
+import ubuntuLogo from '@imgs/tech/ubuntu.svg'
+import mariaDbLogo from '@imgs/tech/mariadb.svg'
+import nginxLogo from '@imgs/tech/Nginx_logo.svg'
+
+export const badges = [
+  {
+    text: 'Million+ NPM PKGs Installed'
+  },
+  {
+    text: '7+ YRS EXP'
+  },
+  {
+    text: '5+ Linux Distros Tried'
+  }
+]
+
+export const techList = [
+  {
+    name: 'koa',
+    logo: koaLogo,
+    url: 'https://koajs.com/'
+  },
+  {
+    name: 'Vuejs',
+    logo: vueLogo,
+    url: 'https://vuejs.org/'
+  },
+  {
+    name: 'postcss',
+    logo: postcssLogo,
+    url: 'https://postcss.org/'
+  },
+  {
+    name: 'HTML',
+    logo: htmlLogo,
+    url: 'https://developer.mozilla.org/en-US/docs/Web/HTML'
+  },
+  {
+    name: 'CSS',
+    logo: cssLogo,
+    url: 'https://developer.mozilla.org/en-US/docs/Web/css'
+  },
+  {
+    name: 'JavaScript',
+    logo: jsLogo,
+    url: 'https://developer.mozilla.org/en-US/docs/Web/javascript'
+  },
+  {
+    name: 'vsCode',
+    logo: vscodeLogo,
+    url: 'https://code.visualstudio.com/'
+  },
+  {
+    name: 'zsh',
+    logo: zshLogo,
+    url: 'https://www.zsh.org/'
+  },
+  {
+    name: 'Docker',
+    logo: dockerLogo,
+    url: 'https://www.docker.com/'
+  },
+  {
+    name: 'Ubuntu',
+    logo: ubuntuLogo,
+    url: 'https://ubuntu.com/'
+  },
+  {
+    name: 'mariaDB',
+    logo: mariaDbLogo,
+    url: 'https://mariadb.org/'
+  },
+  {
+    name: 'nginx',
+    logo: nginxLogo,
+    url: 'https://nginx.org/en/'
+  }
+]
+
+export const expList = [
+  {
+    company: 'Redirect Health',
+    title: 'Senior Full Stack Engineer',
+    fromYr: 2019,
+    endYr: 0,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla. Sodales ut eu sem integer vitae justo eget. Vitae nunc sed velit dignissim.'
+  },
+  {
+    company: 'Success Engine',
+    title: 'Developer and business consultant',
+    fromYr: 2018,
+    endYr: 2019,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla. Sodales ut eu sem integer vitae justo eget. Vitae nunc sed velit dignissim.'
+  },
+  {
+    company: 'Rocket Media',
+    title: 'Web Developer',
+    fromYr: 2018,
+    endYr: 2018,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla. Sodales ut eu sem integer vitae justo eget. Vitae nunc sed velit dignissim.'
+  },
+  {
+    company: 'I-ology',
+    title: 'Front End Developer',
+    fromYr: 2017,
+    endYr: 2018,
+    desc: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Dictum fusce ut placerat orci nulla. Sodales ut eu sem integer vitae justo eget. Vitae nunc sed velit dignissim.'
+  }
+]
