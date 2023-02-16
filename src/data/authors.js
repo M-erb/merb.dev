a3c460f8bb1ddd0d593cfae6d6700472a867ab61
merb <michael.erb@erbwebdev.com>
2023-02-15 21:51:09 -0700

    feat: add author area on posts

---
+++ src/data/authors.js
@@ -0,0 +1,7 @@
+export default [
+  {
+    name: 'Michael Erb',
+    img: '/profile.jpg',
+    bio: 'Fullstack engineer with 7+ years of experience in creating web solutions that take your business to the next level. Passionate about learning and teaching the details of software engineering. Love my family more though.'
+  }
+]
