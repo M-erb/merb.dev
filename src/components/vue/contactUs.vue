643119c25879f5f0bed195827f6bc9f1c95db837
merb <michael.erb@erbwebdev.com>
2023-02-17 11:24:43 -0700

    WIP: add start of contactus form

---
+++ src/components/vue/contactUs.vue
@@ -0,0 +1,32 @@
+<template>
+  <h3>Contact Us!</h3>
+
+  <form @submit.prevent="submitForm">
+    <input type="text" v-model="fields.firstName" />
+
+    <button class="">Submit</button>
+  </form>
+
+  <div class="letter">
+    <p>{{ fields.firstName }}</p>
+  </div>
+</template>
+
+<script>
+export default { name: 'ContactUs' }
+</script>
+
+<script setup>
+import { reactive } from 'vue'
+
+const fields = reactive({
+  firstName: '',
+  lastName: '',
+  email: '',
+  companyName: '',
+})
+
+function submitForm () {
+  console.log('submitForm: ', fields.firstName)
+}
+</script>
--- src/data/authors.js
+++ src/data/authors.js
@@ -2,6 +2,6 @@ export default [
   {
     name: 'Michael Erb',
     img: '/profile.jpg',
-    bio: 'Fullstack engineer with 7+ years of experience in creating web solutions that take your business to the next level. Passionate about learning and teaching the details of software engineering. Love my family more though.'
+    bio: 'Fullstack engineer with 7+ years of experience in creating web solutions that take your business to the next level. Passionate about learning and teaching the details of software engineering, and love my family.'
   }
 ]
