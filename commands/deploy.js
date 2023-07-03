1803015b69a10f2837ed9560c9342cc46ef6d9c9
merb <michael.erb@erbwebdev.com>
2023-07-03 16:27:42 -0600

    fix: remove unused import

--- commands/deploy.js
+++ commands/deploy.js
@@ -1,7 +1,6 @@
 // -------------------
 // Ship off built files
 // -------------------
-import fs from 'node:fs'
 import path from 'node:path'
 import chalk from 'chalk'
 import inquirer from 'inquirer'
