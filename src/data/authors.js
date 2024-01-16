eb75fad578664255ec3b9324aa8558ae2c63e08a
merb <michael.erb@erbwebdev.com>
2024-01-16 14:54:30 -0700

    feat: use astro Image component

--- src/data/authors.js
+++ src/data/authors.js
@@ -1,7 +1,9 @@
+import ProfileImg from '@imgs/profile.jpg'
+
 export default [
   {
     name: 'Michael Erb',
-    img: '/profile.jpg',
+    img: ProfileImg,
     bio: 'Fullstack engineer with 7+ years of experience in creating web solutions that take your business to the next level. Passionate about learning and teaching the details of software engineering, and love my family.'
   }
 ]
