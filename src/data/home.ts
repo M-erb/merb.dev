fe4463ece429d096282a335c53537da9903872c8
merb <michael.erb@erbwebdev.com>
2024-01-26 14:06:49 -0700

    update: new job history copy

--- src/data/home.ts
+++ src/data/home.ts
@@ -111,47 +111,47 @@ export const expList = [
     title: 'Full Stack Engineer',
     fromYr: 2019,
     endYr: 0,
-    tech: ['nodejs', 'vuejs', 'HTML', 'CSS', 'JS', 'Ubuntu'],
-    desc: 'Create custom websites, internal tools, applications and APIs for partners'
+    desc: 'Responsible for various development needs ranging from SEO centered websites to internal and external web tools and automate tasks between systems. Created node.js server to generate personalized PDFs and utilize HubSpot APIs to plug into automated marketing campaigns. Created system to generate landing pages for sales partners that ensures accurate plan information and attribution of leads. Created custom personalized form experiences.',
+    tech: ['Node.js', 'Vue.js', 'Koa.js', 'HTML', 'CSS', 'SASS', 'PHP', 'Gitlab', 'MariaDB', 'Webpack', 'Vite', 'PostCSS', 'Nginx', 'Zsh', 'Bash', 'TypeScript', 'Docker', 'Linux', 'REST APIs', 'Git', 'Ubuntu']
   },
   {
     company: 'Freelancer',
     title: 'Full Stack Engineer',
     fromYr: 2019,
-    endYr: 2019,
-    tech: ['nodejs', 'vuejs', 'HTML', 'CSS', 'JS'],
-    desc: 'Contacts for Mozilla and the Wharton School of Business'
+    endYr: 0,
+    desc: 'Consulting and building solutions for small and medium businesses that fit their needs. Most notably, worked for Mozilla creating a react frontend for a community supported feedback system. Created custom personalized form experiences with goals to collect information from users in engaging ways with email notification support and built-in login system for clients to manage and export submissions. Created custom cost effective brochure sites.',
+    tech: [ 'Node.js', 'Koa.js', 'Express.js', 'React', 'Vue.js', 'Webpack', 'PostCSS', 'HTML', 'CSS', 'SASS', 'GitHub', 'GitLab', 'Nginx', 'Zsh', 'Bash', 'MongoDB', 'TypeScript', 'Docker', 'Linux', 'REST APIs', 'Git', 'GraphQL', 'MariaDB']
   },
   {
     company: 'Success Engine',
     title: 'Developer and business consultant',
     fromYr: 2018,
     endYr: 2019,
-    tech: ['Laravel', 'vuejs'],
-    desc: 'Worked to create custom app for storing zoom data and consulted with small business clients on how to better customize tech for their business'
+    desc: 'Created a custom app for storing Zoom video data for internal use, and to publish on Zoom marketplace. Consulted with small business clients to develop custom designed processes tailored to their business. Taught how to use automation tools like Keap (Infusionsoft).',
+    tech: ['Laravel', 'PHP', 'Node.js', 'Vue.js', 'Webpack', 'HTML', 'CSS', 'SASS', 'OAuth', 'Bash', 'BitBucket']
   },
   {
     company: 'Rocket Media',
     title: 'Full Stack Web Developer',
     fromYr: 2018,
     endYr: 2018,
-    tech: ['PHP(Craft CMS)', 'vuejs', 'JS', 'HTML', 'CSS(SASS)'],
-    desc: 'Created blazing fast and high performing websites for the HVAC industry'
+    desc: 'Worked with a team to design and build measurably valuable web sites for our clients. Focused on creating blazing fast and high performing websites for the HVAC industry. Met with clients to walk through usage of their new sites and tools.',
+    tech: ['PHP', 'CraftCMS', 'Node.js', 'Vue.js', 'HTML', 'CSS', 'SASS', 'PostCSS', 'Nginx', 'Zsh', 'Bash', 'Linux', 'Docker', 'REST APIs', 'GitLab', 'MariaDB', 'Webpack']
   },
   {
     company: 'I-ology',
     title: 'Jr Front End Developer',
     fromYr: 2017,
     endYr: 2018,
-    tech: ['Angularjs (v1)', 'vuejs', 'C#'],
-    desc: 'Worked with a small team to create websites and interfaces for large medical companies like sonoraquest.com'
+    desc: 'Worked with a team to plan and develop websites and usable interfaces to connect clients and their customers with technology. Created interfaces for many different clients including large medical companies like Sonora Quest. Created internal dashboards with thought-out reporting. Created frontend for pharmaceutical purchasing system, a very customized eCommerce of sorts but for pharmaceutical professionals.',
+    tech: ['Angular.js/v1', 'Vue.js', 'SASS', 'C#', 'Razor', 'TypeScript', 'Webpack', 'HTML', 'CSS', 'SASS', 'PostCSS', 'REST APIs', 'Github', 'GraphQL', 'MariaDB']
   },
   {
     company: 'Freelancer',
-    title: 'Front End Developer',
+    title: 'Freelance Web Developer',
     fromYr: 2016,
     endYr: 2017,
-    tech: ['Angularjs (v1)', 'HTML', 'CSS', 'JS', 'nodejs'],
-    desc: 'Did odd jobs for small businesses I came in contact with and through freelancer websites like Fiverr.com'
+    desc: 'Put web technology solutions to work for small and large businesses which brought growth and success to my clients. Created small custom brochure sites for happy clients. Created reporting dashboards by pulling data together from several systems such as netsuite, infusionsoft(keap), and salesforce, utilizing their REST API\'s.',
+    tech: ['Node.js', 'Koa.js', 'Express.js', 'Webpack', 'HTML', 'CSS', 'SASS', 'PostCSS', 'GitHub', 'Nginx', 'Zsh', 'Bash', 'MariaDB', 'MongoDB', 'Linux', 'REST APIs', 'GitLab']
   }
 ]
